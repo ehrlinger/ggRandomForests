4263fca8edffa45f0f760090e3814b61528c24ab
John Ehrlinger <john.ehrlinger@gmail.com>
2014-09-10 14:47:46 -0400

    Add some additional test that functions.
    They are not complete, and do not work.

---
+++ tests/testthat/test_gg_partial.R
@@ -0,0 +1,89 @@
+# testthat for gg_partial function
+context("gg_partial tests")
+
+test_that("gg_partial classifications",{
+  ## IF we want to build the forest every time...
+  #   iris_rf <- rfsrc(Species ~ ., data = iris)
+  # iris_rf <- var.select(iris_rf)
+  
+  ## Load the cached forest
+  data(iris_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(iris_rf, "rfsrc")
+  
+  # Test the forest family
+  expect_that(iris_rf$family, equals("class"))
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_partial(iris_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_partial")
+  
+  # Test varselect is the same
+ #expect_equivalent(select(ggrf.obj$varselect, -names), iris_rf$importance)
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_partial(ggrf.obj)
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
+
+
+test_that("gg_partial survival",{
+  
+  ## IF we want to build the forest every time...
+  #   data(veteran, package = "randomForestSRC")
+  #   veteran_rf <- rfsrc(Surv(time, status) ~ ., data = veteran, ntree = 100)
+  #   veteran_rf <- var.select(veteran_rf)
+  ## Load the cached forest
+  data(veteran_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(veteran_rf, "rfsrc")
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_partial(veteran_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_partial")
+  
+  # Test varselect is the same
+  expect_equal(ggrf.obj$VIMP, as.vector(sort(veteran_rf$importance, decreasing=TRUE)))
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_partial(ggrf.obj)
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
+
+test_that("gg_partial regression",{
+  
+  ## IF we want to build the forest every time...
+  #   ## New York air quality measurements
+  #   airq.obj <- rfsrc(Ozone ~ ., data = airquality, na.action = "na.impute")
+  #   airq_rf <- var.select(airq_rf)
+  ## Load the cached forest
+  data(airq_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(airq_rf, "rfsrc")
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_partial(airq_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_partial")
+  
+  # Test varselect is the same
+  expect_equal(ggrf.obj$VIMP, as.vector(sort(airq_rf$importance, decreasing=TRUE)))
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_partial(ggrf.obj)
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
---
+++ tests/testthat/test_gg_variable.R
@@ -0,0 +1,89 @@
+# testthat for gg_variable function
+context("gg_variable tests")
+
+test_that("gg_variable classifications",{
+  ## IF we want to build the forest every time...
+  #   iris_rf <- rfsrc(Species ~ ., data = iris)
+  # iris_rf <- var.select(iris_rf)
+  
+  ## Load the cached forest
+  data(iris_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(iris_rf, "rfsrc")
+  
+  # Test the forest family
+  expect_that(iris_rf$family, equals("class"))
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_variable(iris_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_variable")
+  
+  # Test varselect is the same
+ #expect_equivalent(select(ggrf.obj$varselect, -names), iris_rf$importance)
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_variable(ggrf.obj, )
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
+
+
+test_that("gg_variable survival",{
+  
+  ## IF we want to build the forest every time...
+  #   data(veteran, package = "randomForestSRC")
+  #   veteran_rf <- rfsrc(Surv(time, status) ~ ., data = veteran, ntree = 100)
+  #   veteran_rf <- var.select(veteran_rf)
+  ## Load the cached forest
+  data(veteran_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(veteran_rf, "rfsrc")
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_variable(veteran_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_variable")
+  
+  # Test varselect is the same
+  expect_equal(ggrf.obj$VIMP, as.vector(sort(veteran_rf$importance, decreasing=TRUE)))
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_variable(ggrf.obj)
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
+
+test_that("gg_variable regression",{
+  
+  ## IF we want to build the forest every time...
+  #   ## New York air quality measurements
+  #   airq.obj <- rfsrc(Ozone ~ ., data = airquality, na.action = "na.impute")
+  #   airq_rf <- var.select(airq_rf)
+  ## Load the cached forest
+  data(airq_rf, package="ggRandomForests")
+  
+  # Test the cached forest type
+  expect_is(airq_rf, "rfsrc")
+  
+  ## Create the correct gg_error object
+  ggrf.obj<- gg_variable(airq_rf)
+  
+  # Test object type
+  expect_is(ggrf.obj, "gg_variable")
+  
+  # Test varselect is the same
+  expect_equal(ggrf.obj$VIMP, as.vector(sort(airq_rf$importance, decreasing=TRUE)))
+  
+  ## Test plotting the gg_error object
+  gg.obj <- plot.gg_variable(ggrf.obj)
+  
+  # Test return is s ggplot object
+  expect_is(gg.obj, "ggplot")
+})
