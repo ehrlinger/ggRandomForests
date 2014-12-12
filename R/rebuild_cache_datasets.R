e5a44fa3c936b55de415370a599c6e9e4d02dd98
John Ehrlinger <john.ehrlinger@gmail.com>
2014-12-12 15:00:44 -0500

    1.1.1 release candidate submission to can
    Correct things all around. Increase ntree in cached examples, and add
    figure captions that are descriptive in vignette. More to do still, but
    “early and often”.

--- R/rebuild_cache_datasets.R
+++ R/rebuild_cache_datasets.R
@@ -3,6 +3,8 @@
 #' @param set Defaults to all sets (NA), however for individual sets specify one 
 #' or more of c("airq", "iris", "mtcars", "pbc", "veteran")
 #' @param save Defaults to write files to the current data directory.
+#' @param pth the directory to store files.
+#' @param ... extra arguments passed to randomForestSRC functions.
 #' 
 #' ggRandomForests operates directly on randomForestSRC objects. In order to 
 #' improve performance on function examples and package tests and checks,
@@ -22,67 +24,82 @@
 #' }
 #'
 #'
-rebuild_cache_datasets <- function(set=NA, save=TRUE){
+rebuild_cache_datasets <- function(set=NA, save=TRUE, pth, ...){
   dta <- new.env()
+  
+  if(missing(pth)){
+  pth <- if(file.exists("data")){
+    if(file.info("data")$isdir){
+      "data/"
+    }else{
+      "./"
+    }
+  }else{
+    "./"
+  } 
+  }else if(!file.info("data")$isdir){
+    stop("Provided path does not exist, or is not a directory.")
+  }
+    
   if(is.na(set))
     set <- c("airq", "iris", "mtcars", "pbc", "veteran")
   
   if("airq" %in% set){
     cat("airq: randomForest\n")
-    airq_rf <- rfsrc(Ozone ~ ., data = airquality, na.action = "na.impute")
-    if(save) save(airq_rf, file="data/airq_rf.rda")
+    airq_rf <- rfsrc(Ozone ~ ., data = airquality, na.action = "na.impute", ...)
+    if(save) save(airq_rf, file=paste(pth, "airq_rf.rda", sep=""), compress="xz")
     
     cat("airq: RF minimal depth\n")
     airq_vs <- var.select(airq_rf)
-    if(save) save(airq_vs, file="data/airq_vs.rda")
+    if(save) save(airq_vs, file=paste(pth, "airq_vs.rda", sep=""), compress="xz")
     
     cat("airq: RF interactions\n")
     airq_interaction <- find.interaction(airq_rf)
-    if(save) save(airq_interaction, file="data/airq_interaction.rda")
+    if(save) save(airq_interaction, file=paste(pth, "airq_interaction.rda", sep=""), compress="xz")
     
     cat("airq: RF partial dependence\n")
     airq_prtl <- plot.variable(airq_rf,
                                partial=TRUE, show.plots=FALSE)
-    if(save) save(airq_prtl, file="data/airq_prtl.rda", compress="xz")
+    if(save) save(airq_prtl, file=paste(pth, "airq_prtl.rda", sep=""), compress="xz")
   }
   
   if("iris" %in% set){
     cat("iris: randomForest\n")
-    iris_rf <- rfsrc(Species ~., data = iris)
-    if(save) save(iris_rf, file="data/iris_rf.rda", compress="xz")
+    iris_rf <- rfsrc(Species ~., data = iris, ...)
+    if(save) save(iris_rf, file=paste(pth, "iris_rf.rda", sep=""), compress="xz")
     
     cat("iris: RF minimal depth\n")
     iris_vs <- var.select(iris_rf)
-    if(save) save(iris_vs, file="data/iris_vs.rda", compress="xz")
+    if(save) save(iris_vs, file=paste(pth, "iris_vs.rda", sep=""), compress="xz")
     
     cat("iris: RF interactions\n")
     iris_interaction <- find.interaction(iris_rf)
-    if(save) save(iris_interaction, file="data/iris_interaction.rda", compress="xz")
+    if(save) save(iris_interaction, file=paste(pth, "iris_interaction.rda", sep=""), compress="xz")
     
     cat("iris: RF partial dependence\n")
     iris_prtl <- plot.variable(iris_rf,
                                partial=TRUE, show.plots=FALSE)
-    if(save) save(iris_prtl, file="data/iris_prtl.rda", compress="xz")
+    if(save) save(iris_prtl, file=paste(pth, "iris_prtl.rda", sep=""), compress="xz")
   }
   
   
   if("mtcars" %in% set){
     cat("mtcars: randomForest\n")
-    mtcars_rf <- rfsrc(mpg ~ ., data = mtcars)
-    if(save) save(mtcars_rf, file="data/mtcars_rf.rda", compress="xz")
+    mtcars_rf <- rfsrc(mpg ~ ., data = mtcars, ...)
+    if(save) save(mtcars_rf, file=paste(pth, "mtcars_rf.rda", sep=""), compress="xz")
     
     cat("mtcars: RF minimal depth\n")
     mtcars_vs <- var.select(mtcars_rf)
-    if(save) save(mtcars_vs, file="data/mtcars_vs.rda", compress="xz")
+    if(save) save(mtcars_vs, file=paste(pth, "mtcars_vs.rda", sep=""), compress="xz")
     
     cat("mtcars: RF interactions\n")
     mtcars_interaction <- find.interaction(mtcars_rf)
-    if(save) save(mtcars_interaction, file="data/mtcars_interaction.rda", compress="xz")
+    if(save) save(mtcars_interaction, file=paste(pth, "mtcars_interaction.rda", sep=""), compress="xz")
     
     cat("mtcars: RF partial dependence\n")
     mtcars_prtl <- plot.variable(mtcars_rf,
                                  partial=TRUE, show.plots=FALSE)
-    if(save) save(mtcars_prtl, file="data/mtcars_prtl.rda", compress="xz")
+    if(save) save(mtcars_prtl, file=paste(pth, "mtcars_prtl.rda", sep=""), compress="xz")
   }
   
   if("pbc" %in% set){
@@ -126,29 +143,29 @@ rebuild_cache_datasets <- function(set=NA, save=TRUE){
     
     cat("pbc: randomForest\n")
     pbc_rf <- rfsrc(Surv(years, status) ~ ., dta$pbc, nsplit = 10,
-                    na.action="na.impute", 
-                    ntree=500)
-    if(save) save(pbc_rf, file="data/pbc_rf.rda", compress="xz")
+                    na.action="na.impute", ...)
+    if(save) save(pbc_rf, file=paste(pth, "pbc_rf.rda", sep=""), compress="xz")
     
     cat("pbc: RF minimal depth\n")
     pbc_vs <- var.select(pbc_rf)
-    if(save) save(pbc_vs, file="data/pbc_vs.rda", compress="xz")
+    if(save) save(pbc_vs, file=paste(pth, "pbc_vs.rda", sep=""), compress="xz")
     
     cat("pbc: RF interactions\n")
     pbc_interaction <- find.interaction(pbc_rf)
-    if(save) save(pbc_interaction, file="data/pbc_interaction.rda", compress="xz")
+    if(save) save(pbc_interaction, file=paste(pth, "pbc_interaction.rda", sep=""), compress="xz")
     
-    # Calculate the 1 year partial dependence
+    # Calculate the partial dependence
+    cat("pbc: RF partial plots (this will take a little while...)\n")
     xvar <- pbc_vs$topvars[1:6]
     
-    pbc_prtl_time <- mclapply(c(1,3,5), function(tm){
+    pbc_prtl_time <- lapply(c(1,3,5), function(tm){
       plot.variable(pbc_rf, surv.type = "surv", 
                     time = tm, 
                     xvar.names = xvar, partial = TRUE, 
                     show.plots = FALSE)
     })
     
-    save(pbc_prtl_time, file="data/pbc_prtl_time.rda", compress="xz")
+    save(pbc_prtl_time, file=paste(pth, "pbc_prtl_time.rda", sep=""), compress="xz")
     
   }
   
@@ -183,23 +200,23 @@ rebuild_cache_datasets <- function(set=NA, save=TRUE){
     dta$veteran <- vet
   
     cat("veteran: randomForest\n")
-    veteran_rf <- rfsrc(Surv(time, status) ~ ., data = dta$veteran, ntree = 100)
+    veteran_rf <- rfsrc(Surv(time, status) ~ ., data = dta$veteran, ...)
     
-    if(save) save(veteran_rf, file="data/veteran_rf.rda", compress="xz")
+    if(save) save(veteran_rf, file=paste(pth, "veteran_rf.rda", sep=""), compress="xz")
     
     cat("veteran: RF minimal depth\n")
     veteran_vs <- var.select(veteran_rf)
-    if(save) save(veteran_vs, file="data/veteran_vs.rda", compress="xz")
+    if(save) save(veteran_vs, file=paste(pth, "veteran_vs.rda", sep=""), compress="xz")
     
     cat("veteran: RF interactions\n")
     veteran_interaction <- find.interaction(veteran_rf)
-    if(save) save(veteran_interaction, file="data/veteran_interaction.rda", compress="xz")
+    if(save) save(veteran_interaction, file=paste(pth, "veteran_interaction.rda", sep=""), compress="xz")
     
     cat("veteran: RF partial dependence\n")
     veteran_prtl <- plot.variable(veteran_rf, surv.type = "surv",
                                   partial = TRUE, time=30,
                                   show.plots=FALSE)
     
-    if(save) save(veteran_prtl, file="data/veteran_prtl.rda", compress="xz")
+    if(save) save(veteran_prtl, file=paste(pth, "veteran_prtl.rda", sep=""), compress="xz")
   }
 }
--- cran-comments.md
+++ cran-comments.md
@@ -1,10 +1,11 @@
 This is the ggRandomForests package submission version 1.1.1
-
 --------------------------------------------------------------------------------
-
+Maintenance release, mostly to fix gg_survival and gg_partial plots.
+* Fix the gg_survival functions to plot kaplan-meier estimates.
+* Fix the gg_partial functions for categorical variables.
+* Add some more S3 print functions.
+* Try to make gg_functions more consistent.
+* Further development of package vignette "Survival with Random Forests" 
+* Modify the example cached datasets for better estimates and examples.
 * Improve help files.
-* combine gg_partial datasets.
-* add panel plotting for multiple gg_variable and gg_partial plots.
-* add a rebuild_cache_data function to ease package dependency issues.
-* Additional examples and test data and codes.
-* Include prelimary vignette.
+* Misc Bug Fixes.
