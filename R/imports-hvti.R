f21c91d44e6611776733255a309ce8778b01e806
John Ehrlinger <john.ehrlinger@gmail.com>
2026-03-03 16:41:13 -0500

    Remove hvtiRutilities dependency and associated imports

--- R/imports-hvti.R
+++
@@ -1,7 +0,0 @@
-#' Internal imports for hvtiRutilities helpers
-#'
-#' ggRandomForests now relies on hvtiRutilities for the r_data_types helper
-#' that was previously vendored in this package.
-#'
-#' @importFrom hvtiRutilities r_data_types
-NULL
